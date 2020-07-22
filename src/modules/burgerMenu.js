f2d54cb4353b02162953150049e56944be1192b9
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-07-22 19:23:53 +0300

    start project

---
+++ src/modules/burgerMenu.js
@@ -0,0 +1,5 @@
+const burgerMenu = () => {
+    console.log(2);   
+}
+
+export default burgerMenu;
---
+++ src/script.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log(1);
+
+import burgerMenu from './modules/burgerMenu.js';
+
+burgerMenu();
