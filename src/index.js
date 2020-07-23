54012c0bc91a3b09869e7eabf978fbf9e22c3287
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-07-23 19:29:41 +0300

    add FormShow

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 'use strict';
+const freeVisit = document.querySelector('.open-popup');
+const freeVisitForm = 'free_visit_form';
+const callbackBtn = document.querySelector('.callback-btn');
+const callBackForm = 'callback_form';
+
+
+
 
 import burgerMenu from './modules/burgerMenu';
+import scrollAction from './modules/scroll';
+import showModalWindow from './modules/showForms';
+
 
 burgerMenu();
-
-import scrollAction from './modules/scroll';
-
 scrollAction();
+showModalWindow(freeVisit, freeVisitForm);
+showModalWindow(callbackBtn, callBackForm);
