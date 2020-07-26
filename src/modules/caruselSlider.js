12e14e53f8851c3abaf242d46b001468eea73016
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-07-26 21:01:44 +0300

    Final end

--- src/modules/caruselSlider.js
+++ src/modules/caruselSlider.js
@@ -5,32 +5,26 @@ const caruselSlider = () => {
     const next = container.querySelector('#next');
     const prev = container.querySelector('#prev');
 
-    let count = 0;
     let left = 0;
     
      slide.style.cssText = `transition: all 0.2s linear 0s; min-width: 210px; margin-left: ${left}px`;
 
     const nextSlider = () => {
-        count++
-        if (count < 6) {
+        if (left < 1125) {
            left += 225;
         slide.style.cssText = `transition: all 0.2s linear 0s; margin-left: -${left}px`; 
         
         } else {
-            count = 0;
             left = 0;
             slide.style.cssText = `transition: all 0.2s linear 0s; margin-left: -${left}px`;
             
         }
                       
     }
-    const prevSlider = () => {
-        
-        count--
-        if(count > 0) {
+    const prevSlider = () => {        
             left += -225
             slide.style.cssText = `transition: all 0.2s linear 0s; min-width: 210px; margin-left: -${left}px`;
-        }   
+           
     }
     prev.addEventListener('click', prevSlider);
     next.addEventListener('click', nextSlider);
